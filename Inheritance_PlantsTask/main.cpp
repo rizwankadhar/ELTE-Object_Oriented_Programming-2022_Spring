c03c0719845ae7e2a37069924635edc355e4e5f8
Rizwan Hussain <rizwankadhar@gmail.com>
2022-04-24 14:28:13 +0200

    Inheritance in OOP-Plants Task

---
+++ Inheritance_PlantsTask/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "Gardener.h"
+using namespace std;
+
+
+int main()
+{
+    Garden* garden = new Garden(5);
+    Gardener* gardener = new Gardener(garden);
+
+    gardener->garden->plant(2, Potatoe::getInstance(), 3);
+    gardener->garden->plant(3, Rose::getInstance(), 4);
+
+    cout << "IDs of parcels, where vegies are ready to harvest:\n";
+
+    vector<int> result = gardener->garden->canHarvest(7);
+
+    for (int i : result)
+    {
+        cout << i << " ";
+    }
+
+    Potatoe::destroy();
+    Rose::destroy();
+	return 0;
+}
+//
+//
+//class Base {
+//  public:
+//    int x;
+//  protected:
+//    int y;
+//  private:
+//    int z;
+//};
+//
+//class PublicDerived: public Base {
+//  // x is public
+//  // y is protected
+//  // z is not accessible from PublicDerived
+//};
+//
+//class ProtectedDerived: protected Base {
+//  // x is protected
+//  // y is protected
+//  // z is not accessible from ProtectedDerived
+//};
+//
+//class PrivateDerived: private Base {
+//  // x is private
+//  // y is private
+//  // z is not accessible from PrivateDerived
+//};
+//
+//class anotherClass : private PrivateDerived {
+//  // x is public
+//  // y is protected
+//  // z is not accessible from PublicDerived
+//};
